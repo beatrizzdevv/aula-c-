b43bbf12044f1031c9931c627f186d1186ea38a4
Beatriz <beatrizdasilvaamorim24@gmail.com>
2026-08-19 15:25:38 -0300

    feat: Criando as declarações do projeto

---
+++ programa01/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace programa01
+{
+     class Bike
+    {
+      //DECLARAÇÕES
+      public string? Modelo { get; set; }
+      //public: metodo de acesso
+      public double Bateria { get; set; }
+        
+      public double PesoCiclista { get; set;}
+        
+      public int RecargaMes { get; set; }
+
+    }
+}
